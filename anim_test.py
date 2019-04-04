e0cce6a81ca47492601beccef02441fa64d92a21
Austin Keller <45545037+kell1353@users.noreply.github.com>
2019-04-04 16:14:05 -0700

    Update anim_test.py

--- anim_test.py
+++ anim_test.py
@@ -2,26 +2,43 @@ from mayavi import mlab
 import numpy as np
 import math
 import time
+import vtk
+import os, os.path
+
+path = "/Users/Austin Keller/Desktop/planet textures"
+
+def get_texture(name, texture):   
+        name.actor.actor.mapper.scalar_visibility=False
+        name.actor.enable_texture = True
+        name.actor.tcoord_generator_mode = 'sphere'
+        name.actor.actor.texture = texture
+        cylinder_mapper = name.actor.tcoord_generator
+        cylinder_mapper.prevent_seam = 0
 
 
+f = mlab.figure()
+ellipse_line_rad =  2
 N=20
-
 points_range = 25
+
 phi = np.linspace(0, np.pi, points_range)
 theta = np.linspace(0, 2*np.pi, points_range)
 phi, theta = np.meshgrid(phi, theta)
 
-f = mlab.figure()
+t = np.linspace(0, 20, 200)
+mlab.plot3d(ellipse_line_rad * np.sin(t), ellipse_line_rad * np.cos(t), 0*t, tube_radius=.05)
 
 r = .5
-
-t = np.linspace(0, 20, 200)
-mlab.plot3d(np.sin(t), np.cos(t), 0*t, tube_radius=.05)
-
 x = r * np.sin(phi) * np.cos(theta) 
 y = r * np.sin(phi) * np.sin(theta) 
 z = r * np.cos(phi)
-s = mlab.mesh(x, y, z)
+jupiter = mlab.mesh(x, y, z)
+
+j_texture = vtk.vtkTexture()
+j_textureReader = vtk.vtkJPEGReader()
+j_textureReader.SetFileName(os.path.join(path, 'jupiter_scaled_flip.jpg'))
+j_texture.SetInputConnection(j_textureReader.GetOutputPort())
+get_texture(jupiter, j_texture)
 
 @mlab.animate(delay=100)
 def anim():
@@ -29,7 +46,7 @@ def anim():
     while i < 100:
         time.sleep(.00000001)
         #s.mlab_source.set(x=5 * np.sin(phi) * np.cos(theta) + i, y=5 * np.sin(phi) * np.sin(theta)+ i, z=5 * np.cos(phi) + i)
-        s.mlab_source.set(x = r * np.sin(phi) * np.cos(theta) + np.cos(2*np.pi*i/N), y = r * np.sin(phi) * np.sin(theta) + np.sin(2*np.pi*i/N), z = r * np.cos(phi) + 0)
+        jupiter.mlab_source.set(x = r * np.sin(phi) * np.cos(theta) + (ellipse_line_rad * np.cos(2*np.pi*i/N)), y = r * np.sin(phi) * np.sin(theta) + (ellipse_line_rad * np.sin(2*np.pi*i/N)), z = r * np.cos(phi) + 0)
         i += 1
         yield
 
